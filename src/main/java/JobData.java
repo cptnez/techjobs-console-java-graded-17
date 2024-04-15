5ef86b2857d163b9be22ce2ad8e9108187a8bdf4
cptnez <gtracey24@gmail.com>
2024-04-15 09:56:57 -0500

    Passing second test. Working on duplicates.

--- src/main/java/JobData.java
+++ src/main/java/JobData.java
@@ -5,10 +5,7 @@ import org.apache.commons.csv.CSVRecord;
 import java.io.FileReader;
 import java.io.IOException;
 import java.io.Reader;
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.HashMap;
-import java.util.List;
+import java.util.*;
 
 /**
  * Created by LaunchCode
@@ -83,6 +80,7 @@ public class JobData {
         return jobs;
     }
 
+
     /**
      * Search all columns for the given term
      *
@@ -95,7 +93,19 @@ public class JobData {
         loadData();
 
         // TODO - implement this method
-        return null;
+
+        ArrayList<HashMap<String, String>> jobs = new ArrayList<>();
+
+            for (HashMap<String, String> job : allJobs) {
+                for (Map.Entry<String, String> jobItem : job.entrySet()) {
+                    {
+                        if (jobItem.getValue().contains(value)){
+                            jobs.add(job);
+                        }
+                    }
+                }
+            }
+        return jobs;
     }
 
     /**
